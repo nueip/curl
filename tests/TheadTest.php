15e397bf459212932b2b46cfc5a135b48c4d263b
Gunter <gunter.chou@staff.nueip.com>
2020-04-06 16:26:00 +0800

    [Dev] Thead support setting childMax

--- tests/TheadTest.php
+++ tests/TheadTest.php
@@ -4,9 +4,32 @@ namespace nueip\curl\tests;
 
 use nueip\curl\Thead;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 
 class TheadTest extends TestCase
 {
+    /**
+     * test set childMax
+     */
+    public function testSetChildMax()
+    {
+        $setMax = 8;
+
+        $thead = new Thead;
+
+        $thead->setChildMax($setMax);
+
+        $reflection = new ReflectionClass(get_class($thead));
+
+        $property = $reflection->getProperty('childMax');
+
+        $property->setAccessible(true);
+
+        $childMax = $property->getValue($thead);
+
+        $this->assertEquals($setMax, $childMax);
+    }
+
     /**
      * test multi process
      */
@@ -24,7 +47,7 @@ class TheadTest extends TestCase
             $arguments[] = [random_int(1, 99), random_int(1, 99)];
         }
 
-        $thead = new Thead();
+        $thead = new Thead;
 
         $thead->multiProcess($addMethod, $arguments);
 
